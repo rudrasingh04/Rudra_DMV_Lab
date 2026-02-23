43d151c0c1067e7e2c47ab147d412d0f1db38d8b
rudrasingh04 <rudrasinghofficial04@gmail.com>
2026-02-23 11:45:39 +0530

    Add files via upload

---
+++ linegraph.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+
+x = [1, 2, 3, 4, 5]
+y = [5, 7, 9, 6, 10]
+
+
+plt.plot(x, y)
+
+
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Basic Line Graph")
+
+
+plt.show()
